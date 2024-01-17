98c3b9890c510887537df2ed7129c1c112976887
Malek Alzein <malek.alzein@studentstore.se>
2024-01-17 18:22:03 +0300

    Create startup project

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sp1_e_commerce/app/my_app.dart';
-import 'package:sp1_e_commerce/core/data/repository/post_repository.dart';
-import 'package:sp1_e_commerce/core/data/repository/prodcut_repository.dart';
+
 import 'package:sp1_e_commerce/core/data/repository/shared_prefrence_repository.dart';
 import 'package:sp1_e_commerce/core/services/connectivity_service.dart';
 import 'package:sp1_e_commerce/core/services/location_service.dart';
@@ -18,8 +17,6 @@ Future<void> main() async {
   Get.put(LocationService());
 
   Get.lazyPut(() => SharedPrefrenceRepository());
-  Get.lazyPut(() => PostRepository());
-  Get.lazyPut(() => ProductRepository());
 
   await Get.putAsync(() async {
     return await SharedPreferences.getInstance();
--- lib/ui/views/splash_view/splash_controller.dart
+++ lib/ui/views/splash_view/splash_controller.dart
@@ -1,22 +1,9 @@
 import 'package:get/get.dart';
 import 'package:sp1_e_commerce/ui/shared/utils.dart';
-import 'package:sp1_e_commerce/ui/views/intro_view/intro_view.dart';
-import 'package:sp1_e_commerce/ui/views/landing_view/landing_view.dart';
-import 'package:sp1_e_commerce/ui/views/main_view/main_view.dart';
 
 class SplashController extends GetxController {
   @override
-  void onInit() {
-    Future.delayed(Duration(seconds: 2)).then((value) {
-      Get.offAll(storege.getFirstLunch()
-          ? IntroView()
-          : storege.getLoggedIn()
-              ? MainView()
-              : LandingView());
-    });
-
-    super.onInit();
-  }
+  void onInit() {}
 
   void reload() {
     onInit();
